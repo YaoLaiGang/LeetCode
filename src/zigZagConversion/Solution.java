6301ea72c6ff3d14aa91f2bc3234e1dbf4cee561
YaoLaiGang <2046682604@qq.com>
2019-02-15 11:38:19 +0800

    动态规划的典型题目，另有字符串的典型题目

--- src/zigZagConversion/Solution.java
+++ src/zigZagConversion/Solution.java
@@ -1,5 +1,7 @@
 package zigZagConversion;
 
+import java.util.ArrayList;
+
 public class Solution {
 /**
  * @author peter
@@ -12,13 +14,41 @@ public class Solution {
  * 输出："PAHNAPLSIIGYIR"
  * 
  * 解决方案：
- * ①使用StringBulider类，把(i+1)/(nRows)==0的字符取出并保存
- * ②循环nRows次，第i次分别取（i+1+nRows*j）-1
- *   当i==nRows/2时,每次取一个①外，还要间隔取出①中所存储的字符串
+ * 注意到每个元素对应的行数是一个循环的先递增又递减的数列
+ * PAYPALISHIRING
+ * 12321232123212
+ * PAYPALISHIRING
+ * 12343212343212
+ * 
  * */
+	public static String convert2(String s, int numRows) {
+		if (numRows <=1) return s;
+		int[] rows = new int[s.length()];
+		char[] sc = s.toCharArray();
+		StringBuilder[] aList = new StringBuilder[numRows];
+		for(int i = 0; i<numRows; ++i){
+			aList[i] = new StringBuilder();
+		}
+		int j;
+		// 填充摆动数列， 其周期为2×numRows-2
+		for(int i=0; i<rows.length; ++i){
+			j = i % (2*numRows - 2) + 1;
+			if (i <= 2*numRows-3) {
+				rows[i] = j>numRows?2*numRows-j:j;
+				aList[rows[i]-1].append(sc[i]);
+			}else {
+				rows[i] = rows[i-(2*numRows-2)];
+				aList[rows[i-(2*numRows-2)]-1].append(sc[i]);
+			}
+		}
+		for(int i = 1; i<numRows; ++i){
+			aList[0].append(aList[i]);
+		}
+		return aList[0].toString();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(convert("ABC", 2));
+		System.out.println(convert2("PAYPALISHIRING", 4));
 	}
     public static String convert(String s, int numRows) {
         StringBuilder temp,inputAfter,result;
