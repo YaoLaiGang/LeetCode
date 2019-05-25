7008b3af88acd47f4d685c6598f4372de59daf59
YaoLaiGang <2046682604@qq.com>
2019-05-25 21:35:35 +0800

    增加了两种方法，一种是递归，一种是牛顿迭代法

--- src/sqrt/Solution.java
+++ src/sqrt/Solution.java
@@ -4,11 +4,43 @@ package sqrt;
 * */
 public class Solution {
     public int mySqrt(int x) {
+        //这种方法太鸡贼了，没啥意思
         return (int)Math.sqrt(x);
     }
-
+    public int mySqrt2(int x){
+        if (x==1) return 1;
+        //二分法
+        long left = 0, right = x, mid;
+        while (right-left>1){
+            System.out.println("letf"+left+"right"+right);
+            mid = (left+right)/2;
+            long mid2 = mid*mid;
+            if (mid2==x) return (int) mid;
+            else if(mid2>x){
+                right = mid;
+            }else {
+                left = mid;
+            }
+        }
+        return (int)left;
+    }
+    public int mySqrt3(int x){
+        /*
+        * 牛顿迭代法
+        * 求出根号a的近似值：首先随便猜一个近似值x，然后不断令x等于x和a/x的平均数，
+        * 迭代个六七次后x的值就已经相当精确了。
+         * */
+        float last, val = x; // 上次迭代的结果
+        do {
+            last = val;
+            val = (val+x/val)/2;
+        }while (Math.abs(val-last)>1e-5);
+        System.out.println(val);
+        return (int)val;
+    }
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.mySqrt(9));
+        System.out.println(solution.mySqrt3(2147395599));
+//        System.out.println(2147395599+1073697799);
     }
 }
