f5c9b2caaa82820446464f83da6e8c7e6776db1d
YaoLaiGang <2046682604@qq.com>
2020-05-31 18:41:51 +0800

    这问题没啥好说的，链表基本操作CRUD之一

--- src/removeLinkedListElements/Solution.java
+++ src/removeLinkedListElements/Solution.java
@@ -1,4 +1,29 @@
 package removeLinkedListElements;
 
 public class Solution {
+    /*
+    * 这问题没啥好说的，链表基本操作CRUD之一
+    * */
+    public ListNode removeElements(ListNode head, int val) {
+        if (head==null) return null;
+        ListNode h = new ListNode(-1, head), pre = h, p = head;
+        while (p!=null){
+            if (p.val==val){
+                pre.next = p.next;
+//                p.next = null;
+                p = pre.next;
+            }else {
+                pre = pre.next;
+                p = p.next;
+            }
+        }
+        return h.next;
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){this.val=val;}
+    ListNode(int val, ListNode next){this.val=val;this.next=next;}
 }
